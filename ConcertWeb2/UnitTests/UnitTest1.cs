983f1dbe9f7821cd2c96b28cbc7d267ecc7bd2a0
Olia <iamola88093@gmail.com>
2021-06-04 00:55:11 +0300

    Add unit tests

---
+++ ConcertWeb2/UnitTests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConcertWeb2.Controllers;
+using ConcertWeb2.Models;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+namespace ConcertWeb2.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public async void Test1()
+        {
+            // Arrange
+            var optionsBuilder = new DbContextOptionsBuilder<ConcertContext>();
+            optionsBuilder.UseSqlServer("Server=DESKTOP-FPLM1TC; Database=Concert; Trusted_Connection=True; MultipleActiveResultSets=true");
+
+            var controller = new ArtistsController(new ConcertContext(optionsBuilder.Options));
+
+            // Act
+            var result = await controller.GetArtists();
+
+            // Assert
+            Assert.Contains(result.Value, g => g.Name.Equals("Go_A"));
+        }
+    }
+}
---
+++ ConcertWeb2/UnitTests/UnitTest2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConcertWeb2.Controllers;
+using ConcertWeb2.Models;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+namespace ConcertWeb2.UnitTests
+{
+    public class UnitTest2
+    {
+        [Fact]
+        public async void Test1()
+        {
+            // Arrange
+            var optionsBuilder = new DbContextOptionsBuilder<ConcertContext>();
+            optionsBuilder.UseSqlServer("Server=DESKTOP-FPLM1TC; Database=Concert; Trusted_Connection=True; MultipleActiveResultSets=true");
+
+            var controller = new CitiesController(new ConcertContext(optionsBuilder.Options));
+
+            // Act
+            var result = await controller.GetCities();
+
+            // Assert
+            Assert.Contains(result.Value, g => g.Name.Equals("Lviv"));
+        }
+    }
+}
